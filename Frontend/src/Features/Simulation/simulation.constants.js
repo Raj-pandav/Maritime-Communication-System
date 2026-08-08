1601fb9346e1974396e0e6759a742bdbf9f11859
Pandav Raj Rameshbhai <rajpandav8989@gmail.com>
2026-08-08 17:35:32 +0530

    Update simulation.constants.js

--- Frontend/src/Features/Simulation/simulation.constants.js
+++ Frontend/src/Features/Simulation/simulation.constants.js
@@ -1,9 +1,14 @@
 export const API = "https://maritime-communication-system.onrender.com/api";
+
 export const SOCKET_URL = "https://maritime-communication-system.onrender.com";
+
 export const GRID_W = 800;
 export const GRID_H = 600;
 export const COMM_RANGE = 150;
 
 export function getDistance(a, b) {
-  return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+  return Math.sqrt(
+    (a.x - b.x) ** 2 +
+    (a.y - b.y) ** 2
+  );
 }
