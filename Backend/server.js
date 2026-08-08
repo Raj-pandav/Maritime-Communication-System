a784a3e258c41da272d821c5d2c12412ef2c3e80
Pandav Raj Rameshbhai <rajpandav8989@gmail.com>
2026-08-08 15:21:51 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -29,9 +29,11 @@ app.set("io", io)
 // Connect to DB then start server
 connectDB()
   .then(() => {
-    server.listen(3000, () => {
-      console.log("Server is running on port 3000")
-    })
+    const PORT = process.env.PORT || 3000;
+
+    server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
   })
   .catch((err) => {
     console.error("Failed to connect to DB:", err)
