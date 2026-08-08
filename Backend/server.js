238c643cf39d9cf54aac4ccb4e008aaad84d1447
Pandav Raj Rameshbhai <rajpandav8989@gmail.com>
2026-08-08 15:24:41 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,40 +1,41 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const http = require("http")
-const app = require("./src/app")
-const { Server } = require("socket.io")
-const connectDB = require("./src/database/db")
+const dotenv = require("dotenv");
+dotenv.config();
 
-// Create HTTP server and attach Socket.IO
-const server = http.createServer(app)
+const http = require("http");
+const app = require("./src/app");
+const { Server } = require("socket.io");
+const connectDB = require("./src/database/db");
+
+const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST"]
-  }
-})
-
-// Socket.IO connection handler build kiya
-io.on("connection", (socket) => {
-  console.log("Frontend connected:", socket.id)
-
-  socket.on("disconnect", () => {
-    console.log("Frontend disconnected:", socket.id)
-  })
-})
-
-// io access kar sakta hai other files ko 
-app.set("io", io)
-
-// Connect to DB then start server
-connectDB()
-  .then(() => {
-    const PORT = process.env.PORT || 3000;
-
-    server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
+    cors: {
+        origin: [
+            "http://localhost:5173",
+            "https://maritime-communication-system.vercel.app"
+        ],
+        methods: ["GET", "POST"]
+    }
 });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to DB:", err)
-  })
+
+io.on("connection", (socket) => {
+    console.log("Frontend connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Frontend disconnected:", socket.id);
+    });
+});
+
+app.set("io", io);
+
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 3000;
+
+        server.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to DB:", err);
+    });
